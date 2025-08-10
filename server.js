2dd941de6f91c0600c67e057ecc69b9dc60c4fb7
melanch0l <minnyinyiworkmail@gmail.com>
2025-08-10 17:29:26 +0630

    testing

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const http = require('http');
+const url = require( 'url');
 const server = http.createServer((req, res) => {
-    res.end("This is Responed Data");
+    const path = req.url;
+    if (path === '/' || path === '/home'){
+        res.end("This is HOME")
+    }else if (path === '/product'){
+        res.end("This is PRODUCT")
+    }else{
+        res.writeHead(404,{
+            "content-type": "text/html"
+        });
+        res.end("<h1>Error!</h1>");
+    }
 });
 server.listen(8000, '127.0.0.1', () => {
     console.log("Listen at port 8000");     
